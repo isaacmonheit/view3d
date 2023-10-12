1e20871fb6d367e7e4b0ffe2dc662cc9d501b9c1
isaacmonheit <isaacmonheit@gmail.com>
2023-10-12 13:56:09 -0400

    added other model and loading screen

--- script.js
+++ script.js
@@ -4,20 +4,11 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 let scene, camera, renderer, controls, object;
 
-function init() {
-    // Set up the scene and camera
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+function loadObjectFromPath(path, backgroundColor) {
+    showLoadingScreen();
 
-    // Set up the renderer
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    // Load the .obj file
     const loader = new OBJLoader();
-    loader.load('SPHYNX888(3dviewer).obj', function (loadedObject) {
+    loader.load(path, function (loadedObject) {
         const box = new THREE.Box3().setFromObject(loadedObject);
         console.log(box.min, box.max, box.getSize(new THREE.Vector3()));
 
@@ -26,11 +17,33 @@ function init() {
         loadedObject.position.x += (loadedObject.position.x - center.x);
         loadedObject.position.y += (loadedObject.position.y - center.y);
         loadedObject.position.z += (loadedObject.position.z - center.z);
-        // loadedObject.scale.set(0.5, 0.5, 0.5);  // Adjust the scale values as needed
+
+        if (object) {
+            scene.remove(object);
+        }
 
         object = loadedObject;
         scene.add(object);
+         // Set renderer's background color
+        renderer.setClearColor(backgroundColor);
+
+         // Hide the loading screen once everything's ready
+        hideLoadingScreen();
     });
+}
+
+function init() {
+    // Set up the scene and camera
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 15;
+
+    // Set up the renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    loadObjectFromPath('SPHYNX888(3dviewer).obj', '#000000');
     
     const ambientLight = new THREE.AmbientLight(0x404040, 1);
     scene.add(ambientLight);
@@ -39,11 +52,6 @@ function init() {
     directionalLight.position.set(1, 1, 1).normalize();
     scene.add(directionalLight);
     
-    
-
-
-
-
 
     // Set up controls for rotation
     controls = new OrbitControls(camera, renderer.domElement);
@@ -64,5 +72,28 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+function showLoadingScreen() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'flex';
+}
+
+function hideLoadingScreen() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'none';
+}
+
+
+// After initializing your scene and setting everything up
+document.addEventListener("DOMContentLoaded", function() {
+    const thumbnails = document.querySelectorAll("#thumbnails img");
+    thumbnails.forEach(thumb => {
+      thumb.addEventListener("click", function() {
+        const objPath = thumb.getAttribute("data-obj");
+        const bgColor = thumb.getAttribute("data-bg-color");
+        loadObjectFromPath(objPath, bgColor);
+      });
+    });
+});
 
 init();
